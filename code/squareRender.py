3f5b2d5ed43a2e4d35dfc7a2a51f7ad30c7a0cfb
Olivia  Alphonce <Mine@dhcp020-210-064.wireless.buffalo.edu>
2016-11-05 18:33:31 -0400

    found the edges of the thing

--- code/squareRender.py
+++ code/squareRender.py
@@ -11,3 +11,11 @@ img = Image.open("../images/left1.JPG").convert("L")
 imgarr = numpy.array(img) 
 
 mlab.imshow(imgarr)
+
+from skimage import data, io, filters
+
+image =imgarr
+# ... or any other NumPy array!
+edges = filters.sobel(image)
+io.imshow(edges)
+io.show()
