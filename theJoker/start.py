0be27d3cb8628c71387d67d6c127c318c27afb21
Emily Hann <emhann@buffalo.edu>
2016-11-06 09:32:21 -0500

    txt file parser done, randomized joke picked

--- theJoker/start.py
+++ theJoker/start.py
@@ -1,1 +1,21 @@
-print('hullo, world.')
+import random
+
+def dadstantiate(fileName):
+	storage = []
+	with open(fileName) as f:
+		for line in f:
+			line = line.strip('\n')
+			storage.append(line.split(','))
+	f.close
+	return storage
+
+def dadifyMe(storage):
+	pickme = random.randrange(0,len(storage))
+	print(storage[pickme])
+
+def main():
+	storage = dadstantiate('input.txt')
+	dadifyMe(storage)
+
+if __name__ == "__main__":
+    main()
